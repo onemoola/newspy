727ced644ea10c08bbeff0f8f1bc0ca4dbef4b50
Sechaba Mofokeng <sechaba@onemoola.com>
2022-12-24 16:56:20 +0200

    feat: add the initial structure for the rss feeds

--- newspy/rss/models.py
+++ newspy/rss/models.py
@@ -1,7 +1,12 @@
+import logging
+
 from pydantic import BaseModel
 from slugify import slugify
 
 from newspy.models import Publication, Publisher
+from newspy.shared import utils
+
+logger = logging.getLogger(__name__)
 
 
 class ArticleMediaContent(BaseModel):
@@ -25,4 +30,19 @@ class Article(BaseModel):
     published: str
 
     def to_publication(self, publisher: Publisher) -> Publication:
-        return Publication(slug=f"{slugify(publisher.name)}-{slugify(self.title)}")
+        try:
+            return Publication(
+                slug=f"{slugify(publisher.name)}-{slugify(self.title)}",
+                url=self.id,
+                url_to_image=self.media_content[0].url,
+                title=self.title,
+                abstract=self.content[0].value,
+                author=None,
+                publisher=publisher,
+                published=utils.to_datetime(self.published),
+            )
+        except ValueError:
+            logger.warning(
+                msg=f"Failed to parse the article with title: {self.title}, id: {self.id}, publisher: {publisher.name}"
+            )
+            pass
