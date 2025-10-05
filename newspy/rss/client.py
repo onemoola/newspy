cd927c77052b35dfefea93dd8a4573a3bff47274
Sechaba Mofokeng <sechaba@onemoola.com>
2025-10-05 11:35:14 +0200

    fix(client): set User-Agent header for HTTP requests in client

--- newspy/rss/client.py
+++ newspy/rss/client.py
@@ -34,7 +34,9 @@ def get_articles(
                 http_client.send,
                 method=HttpMethod.GET,
                 url=source.url,
-                headers=None,
+                headers={
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
+                },
                 params=None,
                 payload=None,
             )
