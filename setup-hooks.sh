6101782e23ce8902f6bebba72fe1c49b558f86b4
Sechaba Mofokeng <sechaba@onemoola.com>
2025-12-24 13:45:41 +0200

    chore: simplify git hooks setup in setup-hooks.sh

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Configure git to use hooks from the .githooks directory
-git config core.hooksPath .githooks
-
 chmod +x .githooks/commit-msg
 
 if git config core.hooksPath .githooks; then
